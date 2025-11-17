80b3f46695da91d5525fbcb4d2327e1f776b8c06
Andy de Oliveira <elderromero565@gmail.com>
2025-11-17 20:18:04 -0300

    e3

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 origins = [
     "https://ironexecutions.com.br"
-    "https://ironexecutions-backend.onrender.com",
+   "https://ironexecutions-frontend.onrender.com",
     "http://localhost:5173",
     "http://localhost:3000",
     "*"
